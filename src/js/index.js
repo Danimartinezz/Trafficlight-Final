eac5fff4c8c7bbc7652c67644a0bc2f57c8b2e62
Danimartinezz <daniel.martinez094@mymdc.net>
2022-06-22 01:44:08 +0000

    My first commit

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Trafficlight from "/workspace/react-hello/src/js/component/Trafficlight.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Trafficlight />, document.querySelector("#app"));
