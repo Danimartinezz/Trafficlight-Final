96696c1c0a55fe60d7728dac183fbf36917185dc
Danimartinezz <daniel.martinez094@mymdc.net>
2022-06-29 23:38:39 +0000

    My first commit

--- src/js/component/Trafficlight.jsx
+++ src/js/component/Trafficlight.jsx
@@ -1,32 +1,55 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
-export default class Trafficlight extends React.Component{
+// export default class Trafficlight extends React.Component{
 	
-	constructor(){
-		super();
-		this.state ={
-			clickedLight: null
-		}
-	}
+// 	constructor(){
+// 		super();
+// 		this.state ={
+// 			clickedLight: null
+// 		}
+// 	}
 	
-	render(){
-		console.log(this.state);
+// 	render(){
+// 		console.log(this.state);
+// 		let redExtraClass = '';
+// 		if(this.state.clickedLight == 'red') redExtraClass = 'selected';
+// 		let yellowExtraClass = '';
+// 		if(this.state.clickedLight == 'yellow') yellowExtraClass = 'selected';
+// 		let greenExtraClass = '';
+// 		if(this.state.clickedLight == 'green') greenExtraClass = 'selected';
+		
+// 		return <div>
+// 		<div id="trafficTop"></div>
+// 		<div id="container">
+// 			<div className={"red light " +redExtraClass} onClick={() => this.setState({ clickedLight: 'red'})}></div>
+// 			<div className={"yellow light " +yellowExtraClass} onClick={() => this.setState({ clickedLight: 'yellow'})}></div>
+// 			<div className={"green light " +greenExtraClass} onClick={() => this.setState({ clickedLight: 'green'})}></div>
+// 		</div>
+// 		</div>;
+// 	}
+// }
+
+console.log(this.state);
 		let redExtraClass = '';
 		if(this.state.clickedLight == 'red') redExtraClass = 'selected';
 		let yellowExtraClass = '';
 		if(this.state.clickedLight == 'yellow') yellowExtraClass = 'selected';
 		let greenExtraClass = '';
 		if(this.state.clickedLight == 'green') greenExtraClass = 'selected';
-		
-		return <div>
+
+const Trafficlight = () => {
+	const [state, setState] = useState("");
+	return (
+		 <div>
 		<div id="trafficTop"></div>
 		<div id="container">
-			<div className={"red light " +redExtraClass} onClick={() => this.setState({ clickedLight: 'red'})}></div>
-			<div className={"yellow light " +yellowExtraClass} onClick={() => this.setState({ clickedLight: 'yellow'})}></div>
-			<div className={"green light " +greenExtraClass} onClick={() => this.setState({ clickedLight: 'green'})}></div>
+			<div className={"red light " +redExtraClass} onClick={() => setState({ clickedLight: 'red'})}></div>
+			<div className={"yellow light " +yellowExtraClass} onClick={() => setState({ clickedLight: 'yellow'})}></div>
+			<div className={"green light " +greenExtraClass} onClick={() => setState({ clickedLight: 'green'})}></div>
 		</div>
-		</div>;
-	}
-}
+		</div>
+	);
+};
 
-
+export default Trafficlight;
