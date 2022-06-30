723979e6cd7f3fe8505dddf88ed0a3ad07470616
Danimartinezz <daniel.martinez094@mymdc.net>
2022-06-30 00:12:06 +0000

    My first commit

--- src/js/component/Trafficlight.jsx
+++ src/js/component/Trafficlight.jsx
@@ -30,23 +30,24 @@ import React, { useState } from "react";
 // 	}
 // }
 
-console.log(this.state);
-		let redExtraClass = '';
-		if(this.state.clickedLight == 'red') redExtraClass = 'selected';
-		let yellowExtraClass = '';
-		if(this.state.clickedLight == 'yellow') yellowExtraClass = 'selected';
-		let greenExtraClass = '';
-		if(this.state.clickedLight == 'green') greenExtraClass = 'selected';
+
 
 const Trafficlight = () => {
 	const [state, setState] = useState("");
+	console.log(state);
+		let redExtraClass = '';
+		if(state == 'red') redExtraClass = 'selected';
+		let yellowExtraClass = '';
+		if(state == 'yellow') yellowExtraClass = 'selected';
+		let greenExtraClass = '';
+		if(state == 'green') greenExtraClass = 'selected';
 	return (
 		 <div>
 		<div id="trafficTop"></div>
 		<div id="container">
-			<div className={"red light " +redExtraClass} onClick={() => setState({ clickedLight: 'red'})}></div>
-			<div className={"yellow light " +yellowExtraClass} onClick={() => setState({ clickedLight: 'yellow'})}></div>
-			<div className={"green light " +greenExtraClass} onClick={() => setState({ clickedLight: 'green'})}></div>
+			<div className={"red light " +redExtraClass} onClick={() => setState(  'red')}></div>
+			<div className={"yellow light " +yellowExtraClass} onClick={() => setState( 'yellow')}></div>
+			<div className={"green light " +greenExtraClass} onClick={() => setState(  'green')}></div>
 		</div>
 		</div>
 	);
